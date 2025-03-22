f783ef1ec2dc278296b222a2f220166a3d47214c
IdziM <maciej.idzikowski7@gmail.com>
2025-03-22 14:04:03 +0100

    dodanie filtrowania po składanikach

--- backend/src/services/recipe.service.ts
+++ backend/src/services/recipe.service.ts
@@ -44,12 +44,20 @@ export class RecipeService {
     await this.recipeRepo.delete(id);
   }
   // Filtracja przepisów
-   async filterRecipes(filters: { name?: string; category?: string }) {
-    const query = AppDataSource.getRepository(Recipe).createQueryBuilder("recipe");
+  async filterRecipes(filters: { name?: string; category?: string; ingredients?: string[] }) {
+    const query = AppDataSource.getRepository(Recipe)
+      .createQueryBuilder("recipe")
+      .leftJoinAndSelect("recipe.ingredients", "ingredient");
 
-    if (filters.name) query.andWhere("recipe.name LIKE :name", { name: `%${filters.name}%` });
-    if (filters.category) query.andWhere("recipe.category = :category", { category: filters.category });
+    if (filters.name) {
+      query.andWhere("recipe.name LIKE :name", { name: `%${filters.name}%` });
+    }
+
+    if (filters.ingredients && filters.ingredients.length > 0) {
+      query.andWhere("ingredient.name IN (:...ingredients)", { ingredients: filters.ingredients });
+    }
 
     return await query.getMany();
+
   }
 }
