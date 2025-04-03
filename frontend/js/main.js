9701884b058b87673eb3bd7656a968586940f700
IdziM <maciej.idzikowski7@gmail.com>
2025-04-03 18:39:10 +0200

    frontedn - rozbudowa - stworzenie skrytu js/ui/layout

---
+++ frontend/js/main.js
@@ -0,0 +1,10 @@
+import { renderLayout } from "./ui/layout.js";  // Importowanie z folderu 'ui'
+
+
+// Renderowanie layoutu na samym początku
+renderLayout(); 
+
+// Możesz dodać inne funkcje po tym, jak layout jest już gotowy
+document.addEventListener("DOMContentLoaded", () => {
+    // Inne inicjalizacje mogą być tutaj
+});
