8f4aaac7525c41b5f320d51a4f1c3f4ab20edbdf
IdziM <maciej.idzikowski7@gmail.com>
2025-04-06 22:43:01 +0200

    poprawki stylowania navbara

--- frontend/js/ui/layout.js
+++ frontend/js/ui/layout.js
@@ -1,21 +1,23 @@
 export function renderLayout(pageTitle = "") {
     const header = document.createElement("header");
+    header.classList.add("custom-header"); // Dodajemy klasę do stylowania
+
     header.innerHTML = `
-        <nav class="navbar navbar-light bg-light">
+        <nav class="navbar ">
             <div class="container-fluid">
-                <a class="navbar-brand d-flex align-items-center" href="#">
-                    <img src="../assets/logo.png" alt="KittchenBook Logo" width="30" height="30" class="d-inline-block align-text-top me-2">
+                <a class="navbar-brand d-flex align-items-center text-white" href="#">
+                    <img src="../assets/logo.png" alt="KittchenBook Logo" width="30" height="30" class="me-2">
                     ${pageTitle || "KittchenBook"}
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#kittchenNavbar" aria-controls="kittchenNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler text-white border-white" type="button" data-bs-toggle="collapse" data-bs-target="#kittchenNavbar" aria-controls="kittchenNavbar" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="kittchenNavbar">
                     <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><a class="nav-link" href="../../index.html">Strona Główna</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../../pages/recipes.html">Przepisy</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../../pages/mealplanner.html">Planer Posiłków</a></li>
-                        <li class="nav-item"><a class="nav-link" href="../../pages/about.html">O nas</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="../../index.html">Strona Główna</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="../../pages/recipes.html">Przepisy</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="../../pages/mealplanner.html">Planer Posiłków</a></li>
+                        <li class="nav-item"><a class="nav-link text-white" href="../../pages/about.html">O nas</a></li>
                     </ul>
                 </div>
             </div>
@@ -23,7 +25,7 @@ export function renderLayout(pageTitle = "") {
     `;
 
     const footer = document.createElement("footer");
-    footer.classList.add("text-center", "mt-4", "py-3", "bg-light");
+    footer.classList.add("custom-footer", "text-center", "mt-4", "py-3");
     footer.innerHTML = `
         <p class="mb-0">&copy; 2024 KittchenBook. Wszelkie prawa zastrzeżone.</p>
     `;
